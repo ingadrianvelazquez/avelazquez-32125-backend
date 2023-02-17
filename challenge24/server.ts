35ebf45ea383ea8c6d9b6e46233686879450dc1a
ingadrianvelazquez <ing.adrian.velazquez@gmail.com>
2023-02-17 15:08:26 -0300

    Update server.ts

--- challenge24/server.ts
+++ challenge24/server.ts
@@ -8,17 +8,12 @@ console.log(`Server UP on PORT ${port}`);
 let colores = [];
 
 const header = `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<title>DENO Server</title>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1">\n\t<link href="css/style.css" rel="stylesheet">\n</head>\n<body>\n`;
+const form = `<h1>Simple Deno Server Form</h1>\n\t<form id=form method=post onSubmit="upColor()">\n\t\t<input type=text placerholder=color id=color name=color>\n\t\t<input type=submit value=Send>\n\t</form>\n\t`;
 const script = `<script>function upColor() {\n\tlet color = document.getElementById("color").value;\n\tdocument.getElementById("form").action = "/color=" + color;\n}\n</script>\n`;
 const footer = `</body>\n</html>\n`;
 
 const getColors = () => {
     return '<ul>' + colores.map(elem => `<li>${elem}</li>`).join('') + '</ul>';
-}
-
-const showColors = () => {
-    for (const color of colores) {
-        console.log(colors[color](color))
-    }
 }
 
 for await (const req of server) {
@@ -27,16 +22,9 @@ for await (const req of server) {
         const color = req.url.replace('/color=', '');
         colores.push(color);
         console.log(colors[color](color))
-
-        req.respond({
-            body: `${header}<h1>Simple Deno Server Form</h1>\n\t<form id=form method=post onSubmit="upColor()">\n\t\t<input type=text placerholder=color id=color name=color>\n\t\t<input type=submit>\n\t</form>\n\t<pre>${getColors()}</pre>\n${script}${footer}`
-        })
-
-    } else {
-
-        req.respond({
-            body: `${header}<h1>Simple Deno Server Form</h1>\n\t<form id=form method=post onSubmit="upColor()">\n\t\t<input type=text placerholder=color id=color name=color>\n\t\t<input type=submit>\n\t</form>\n\t<pre>${getColors()}</pre>\n${script}${footer}`
-        })
     }
+    req.respond({
+        body: `${header}${form}<pre>${getColors()}</pre>\n${script}${footer}`
+    })
 }
 
